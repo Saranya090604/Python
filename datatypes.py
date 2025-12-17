d97de079038475c244d86c33582e9c187736cdcc
saranya <saranyasanjana09@gmail.com>
2025-12-17 14:47:25 +0530

    datatypes file added

--- datatypes.py
+++ datatypes.py
@@ -1,2 +1,8 @@
-def add(a, b):
-    return a + b
+#Datatypes
+print(type(24))
+print(type(10.5))
+print(type(2e6))
+print(type(2e6j))
+#strings
+print(type("Hello World"))
+print(type('Hello World'))
