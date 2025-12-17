603476a8145e2a0ef823a74b10f8b01713276789
saranya <saranyasanjana09@gmail.com>
2025-12-17 17:10:17 +0530

    variable convention

--- variable.py
+++ variable.py
@@ -1,23 +1,31 @@
-# # # x = 2
-# # # y = 52.7
-# # # print(x)
-# # # print(y)
-# # # print('x')
-# # # print('y')
-# # # print(type(x))
-# # # print(type(y))
+# # # # # x = 2
+# # # # # y = 52.7
+# # # # # print(x)
+# # # # # print(y)
+# # # # # print('x')
+# # # # # print('y')
+# # # # # print(type(x))
+# # # # # print(type(y))
+# # # #
+# # # # x = str(3)
+# # # # y = int(3)
+# # # # z = int(3)
+# # # # print(x)
+# # # # print((type(y)))
+# # # # print(z)
 # #
-# # x = str(3)
-# # y = int(3)
-# # z = int(3)
-# # print(x)
-# # print((type(y)))
-# # print(z)
+# # # var_sar = 28
+# # # print(var_sar)
+# #
+# # age = 10
+# # Age = 20
+# # AGE = 30
+# # print(Age)
+#
+# a, b, c = 'Orange', 'Apple', 'Banana'
+# print(a, b, c, sep= '\n')
 
-# var_sar = 28
-# print(var_sar)
-
-age = 10
-Age = 20
-AGE = 30
-print(Age)
+purchase = 26
+cost = 43
+total_cost = purchase * cost
+print(total_cost)
