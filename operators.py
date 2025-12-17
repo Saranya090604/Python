388b4f0c93b6538912f49cbcaa324d57d4e44a84
saranya <saranyasanjana09@gmail.com>
2025-12-17 22:12:48 +0530

    operators file

---
+++ operators.py
@@ -0,0 +1,61 @@
+#  Arithmetic operations
+# x = 5
+# y = 7
+# print (x + y)
+# print (x - y)
+# print (x * y)
+# print (x / y)
+# print (x % y)
+# print (x // y)
+
+#  Assignment operators
+# x = 15
+# print(x)
+# x ^= 5
+# print(x)
+
+# Comparison operators
+# x = 40
+# print(x == 10)
+# print(x != 10)
+# print(x > 10)
+# print(x >= 10)
+# print(x <= 10)
+# print(x < 10)
+
+# Logical operator
+# x = 20
+# and
+# print (x > 10 and x == 20)
+# or
+# print(x < 20 or x >= 20)
+# not
+# print(not(x >= 15))
+
+# identity operator
+# x = ['apple', 'kiwi']
+# y = ['apple', 'kiwi']
+# z = y
+# print(x is y)
+# print(x is z)
+# print(y is z)
+# print(y is not x)
+
+# membership operator
+# x = 'kiwi'
+# print('a' in x)
+# print('z' not in x)
+
+# Bitwise operator
+x = 4
+y = 5
+result = x & y
+print(result)
+result = x | y
+print(result)
+result = x ^ y
+print(result)
+result = x << 1
+print(result)
+result = x >> y
+print(result)
