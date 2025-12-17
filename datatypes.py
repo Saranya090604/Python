2c16f5aa01d7f9f487e33198c82029871429d622
saranya <saranyasanjana09@gmail.com>
2025-12-17 15:56:48 +0530

    sequence and string added

--- datatypes.py
+++ datatypes.py
@@ -1,8 +1,34 @@
-#Datatypes
-print(type(24))
-print(type(10.5))
-print(type(2e6))
-print(type(2e6j))
-#strings
-print(type("Hello World"))
-print(type('Hello World'))
+# # # #Datatypes
+# # # print(type(24))
+# # # print(type(10.5))
+# # # print(type(2e6))
+# # # print(type(2e6j))
+# # # #strings
+# # # print(type("Hello World"))
+# # # print(type('Hello World'))
+# # print(True)
+# # print(False)
+# # print(type(True))
+# # print(type(False))
+# s1 = 'Hello World'
+# print(s1)
+# print(type(s1))
+# # sequence list
+# l1 = [12, 13, 15, 16]
+# print(l1)
+# print(type(l1))
+# # Tuples
+# l1 = (12, 13, 15, 16)
+# print(l1)
+# print(type(l1))
+
+s1 = {10, 20, 30, 40}
+print(s1)
+print(type(s1))
+# Dictionaries
+d1 = {
+    'Name': 'saran',
+    'Age': 21
+}
+print (d1)
+print(type(d1))
