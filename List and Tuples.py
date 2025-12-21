88c72ae263a5a40c02a18509d8632ffed0c1cecf
saranya <saranyasanjana09@gmail.com>
2025-12-21 11:20:15 +0530

    list and range

---
+++ List and Tuples.py
@@ -0,0 +1,17 @@
+# List : Square brackets
+# ordered, changeable, and Allow duplicates
+# l1 = ['a','b','c']
+# l2 = [1 , 9, 10]
+# l3 = [True, False, True, False]
+# l4 = ['s', 'v', 5, 8, 9.2, 8j, True, False]
+# print(l1, l2, l3, l4, sep='\n')
+# print(type(l1), type(l2), type(l3), type(l4), type(l4), sep='\n')
+
+l1 = list(range(15))
+print(l1)
+
+l2 = list(range(5, 15))
+print(l2)
+
+l3 = list(range(6, 13, 2))
+print(l3)
