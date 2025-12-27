07b9efdab880f08988746970aab04c8932380ff6
saranya <saranyasanjana09@gmail.com>
2025-12-27 22:27:58 +0530

    oops concept basic

---
+++ OOPs Concept.py
@@ -0,0 +1,14 @@
+# create a class
+class Person:
+    # class Attributes(variables)
+    bank_name = 'SBI Bank'
+    #Methods(function)
+    def greet(self):
+        print("Welcome to SBI Bank")
+
+# create a object
+c1 = Person()
+c1.greet()
+
+c2 = Person()
+c2.greet()
