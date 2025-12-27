9466db5666b89c3979223f3089d9c213112d1430
saranya <saranyasanjana09@gmail.com>
2025-12-27 18:46:08 +0530

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+d1 = {
+    'Name': 'saranya',
+    'Age': 21,
+    'Gender': 'Female',
+    'car': ['BMW', 'BENZ'],
+    'Name': 'sanjana'
+}
+print(d1)
+print(type(d1))
+print(len(d1))
+print(d1['Name'])
+
+
